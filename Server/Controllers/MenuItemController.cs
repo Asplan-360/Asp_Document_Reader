0bd88b6dd33492426dd72c2a8b5796d3e80846fc
Thiagogradev25 <98544300+Thiagogradev25@users.noreply.github.com>
2022-04-06 17:40:36 -0300

    Refatoração MenuItemController

--- Server/Controllers/MenuItemController.cs
+++ Server/Controllers/MenuItemController.cs
@@ -10,16 +10,19 @@ public class MenuItemController : ControllerBase
     private readonly IMenuItemService _menuService;
     private readonly ILogger<IMenuItemService> _logger;
 
-    
+
     public MenuItemController(IMenuItemService menuService, ILogger<IMenuItemService> logger)
     {
         _menuService = menuService;
         _logger = logger;
     }
 
+    [HttpGet]
     public async Task<ActionResult<ServiceResponse<List<MenuItem>>>> GetMenuItems()
     {
         var result = await _menuService.GetMenuItems();
         return result;
     }
+
+   
 }
