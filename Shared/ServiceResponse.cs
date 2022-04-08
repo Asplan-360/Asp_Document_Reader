5fd1a8d61735e5c3f42219c686d3e6fcbc3b4e6f
Thiagogradev25 <98544300+Thiagogradev25@users.noreply.github.com>
2022-04-08 15:43:50 -0300

    Create ServiceResponse.cs

---
+++ Shared/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp_Document_Reader.Shared;
+
+internal class ServiceResponse<T>
+{
+
+    public T? DataT { get; set; }
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
+}
