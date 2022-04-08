a2fc6a47de23c72d670216bb1288f52dfc28a39f
Thiagogradev25 <98544300+Thiagogradev25@users.noreply.github.com>
2022-04-08 16:58:17 -0300

    teste

---
+++ Server/Migrations/20220408184604_Teste.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asp_Document_Reader.Server.Migrations
+{
+    public partial class Teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "viewGer_Menu",
+                columns: table => new
+                {
+                    GER_Rotina_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GER_Menu_ID = table.Column<int>(type: "int", nullable: false),
+                    DescMenu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrdemMenu = table.Column<int>(type: "int", nullable: false),
+                    DescRotina = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrdemRotina = table.Column<int>(type: "int", nullable: false),
+                    acesspadm = table.Column<bool>(type: "bit", nullable: false),
+                    icone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    janela = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    rota = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    paramauxiliar = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_viewGer_Menu", x => x.GER_Rotina_Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "viewGer_Menu");
+        }
+    }
+}
