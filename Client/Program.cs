47bf411b106b620dabb43d85299c30ba5504f86b
Thiagogradev25 <98544300+Thiagogradev25@users.noreply.github.com>
2022-04-25 17:02:35 -0300

    ok

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,5 @@
 global using Asp_Document_Reader.Client;
-global using Asp_Document_Reader.Client.Services;
+
 global using Asp_Document_Reader.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IMenuItemService, MenuItemService>();
+
 
 await builder.Build().RunAsync();
--- Client/Services/IMenuItemService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Asp_Document_Reader.Client.Services;
-
-public interface IMenuItemService
-{
-    public List<MenuItem> MenuItems { get;}
-
-   public Task<List<MenuItem>?> GetMenuItems();
-}
